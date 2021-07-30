96f070e3a4000252842ed23b89af40df16e6ed4e
Dawid Malenta <rezorl94@gmail.com>
2021-07-30 17:57:36 +0200

    added ngx-translate

---
+++ src/app/config/translate/translate.config.ts
@@ -0,0 +1,13 @@
+import {HttpClient} from '@angular/common/http';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+
+export const TranslateConfigModule = TranslateModule.forRoot({
+  useDefaultLang: false,
+  defaultLanguage: navigator.language,
+  loader: {
+    provide: TranslateLoader,
+    useFactory: (http: HttpClient) => new TranslateHttpLoader(http, '../assets/i18n/', '.json'),
+    deps: [HttpClient]
+  }
+});
